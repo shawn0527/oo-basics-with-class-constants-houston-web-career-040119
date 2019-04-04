6cac5f69da0ac83dc8bb1cb5e1449879743e40fa
Shawn Ren <shawnren527@gmail.com>
2019-04-04 03:07:08 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
+
+
   def initialize(brand)
     @brand = brand
+    BRANDS.push(@brand).uniq!
   end
 
   def cobble
